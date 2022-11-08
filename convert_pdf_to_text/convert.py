4200fd66b718f5ed175997abb8cd02d1c8a4ea0a
Leticia <leticia.souza@naxi.com.br>
2022-11-08 17:21:46 -0300

    altered

--- convert_pdf_to_text/convert.py
+++ convert_pdf_to_text/convert.py
@@ -1,21 +1,23 @@
 from PyPDF2 import PdfReader
 import os
-import shutil
+from time import sleep
 
-if(os.path.isdir('temp') == False):
+if os.path.isdir('temp') == False:
   os.mkdir('temp')                     # cria um diretório chamado temp, caso ele já não exista
 
 pdf_path = ''
 
 pdf_path = input('Insira o nome de seu arquivo PDF - Use a barra invertida para colocar o caminho de seu diretório: ') # insira o caminho onde está o PDF
+print('Convertendo...')
+sleep(1)
+print('convertido para txt!')
 
 reader = PdfReader(pdf_path) 
 page = reader.pages[0].extract_text() # depois de ler o PDF, vai ser extraído o texto desse arquivo.
 
-txt_pdf = open('convertido.txt', 'a') # cria um arquivo TXT caso não tenha, em modo escrita
+txt_pdf = open('temp/convertido.txt', 'a') # cria um arquivo TXT caso não tenha, em modo escrita
 txt_pdf.write(page) 
 txt_pdf.close()
 
-shutil.move('convertido.txt', 'temp') 
 
 
